d4c7df5a567481decb1700d97e35f203c78a6481
sowmi <meghanakristipati@gmail.com>
2021-03-13 13:31:24 +0530

    second commit

---
+++ CardController.java
@@ -0,0 +1,14 @@
+public class CardController
+{
+public static void main(String args[])
+{
+System.out.println("main method starts");
+Card c1 = new Card( );
+System.out.println(c1.Cardid);
+System.out.println(c1.Cardnumber);
+System.out.println(c1.expirydate);
+System.out.println(c1.cvv);
+System.out.println(c1.Cardtype);
+System.out.println("main method ends");
+}
+}

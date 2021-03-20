322be5ce282e0b005215b6280dbc9ca8c08d5140
sowmi <meghanakristipati@gmail.com>
2021-03-20 13:12:33 +0530

    fullfillment

---
+++ FullFillMentSource.java
@@ -0,0 +1,15 @@
+			public class FullFillMentSource
+		{
+			String nodedid;
+			String actuallnventorynode;
+			  int  quantity;
+			public void setNodedId(String i){
+				nodedid=i;
+		}
+		        public void setActuallNventoryNode(String n){
+				actuallnventorynode=n;
+		}
+		        public void setQuantity(String q){
+				quantity=q;
+		}
+	}		
